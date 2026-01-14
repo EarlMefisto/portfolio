910bd7e9bd92062b8ab9e5f251fc4cc7203b020b
Irina Lebedeva <hataketodoroke@gmail.com>
2026-01-14 21:03:30 +0300

    restoring order in projects.ts

--- src/shared/constants/projects.ts
+++ src/shared/constants/projects.ts
@@ -1,6 +1,6 @@
 import postImage1 from '@/assets/img/0cbd17e4381497547009ce81acc4eee3.webp'
-import postImage2 from '@/assets/img/roulet.webp'
-import postImage3 from '@/assets/img/stone.webp'
+import postImage4 from '@/assets/img/roulet.webp'
+import postImage5 from '@/assets/img/stone.webp'
 
 export const PROJECTS = [
   {
@@ -11,17 +11,19 @@ export const PROJECTS = [
   //   {
   //     href: 'https://earlmefisto.github.io/DNF/',
   //     projectId: 'todolist',
+  //     postImageId: postImage2,
   //   },
   //   {
   //     projectId: 'inctagram',
+  //     postImageId: postImage3,
   //   },
   {
     projectId: 'roulet',
-    postImageId: postImage2,
+    postImageId: postImage4,
   },
   {
     href: 'https://granitvechnosti.ru/',
     projectId: 'granite',
-    postImageId: postImage3,
+    postImageId: postImage5,
   },
 ]
